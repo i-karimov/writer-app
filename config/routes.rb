d0d87eff43ff83b455064f9344b1354646a04c81
Ilmir <code.for.func@gmail.com>
2024-06-26 01:15:35 +0400

    add method controller(posts/delete)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: %i[index new create edit update]
+  resources :posts, only: %i[index new create edit update destroy]
 
   root 'pages#index'
 end
