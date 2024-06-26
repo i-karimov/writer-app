bfa6b7ff8883ae80601a40d358b2d369a1e51e52
Ilmir <code.for.func@gmail.com>
2024-06-26 11:05:53 +0400

    add auth helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,7 @@
 module ApplicationHelper
   def nav_tab(title, url, options = {})
     current_page = options.delete(:current_page)
-
-    options[:class] += ' bg-primary text-white' if current_page == title
-    puts '*' * 30
-    puts title, url, options
-    puts '*' * 30
-    puts current_page
+    options[:class] += ' text-white bg-warning' if current_page == title
 
     link_to title, url, options
   end
