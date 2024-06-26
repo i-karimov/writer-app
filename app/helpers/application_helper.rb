ea2ea8b8c3857154c75a030a46063cf8ca14b531
Ilmir <code.for.func@gmail.com>
2024-06-26 07:35:34 +0400

    add current tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def nav_tab(title, url, options = {})
+    current_page = options.delete(:current_page)
+
+    options[:class] += ' bg-primary text-white' if current_page == title
+    puts '*' * 30
+    puts title, url, options
+    puts '*' * 30
+    puts current_page
+
+    link_to title, url, options
+  end
+
+  def currently_at(current_page = '')
+    render partial: 'shared/menu', locals: { current_page: }
+  end
 end
