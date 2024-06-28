7b4cc2cfaa553bfd77ba86163468c0b5e0bb1af1
Ilmir <code.for.func@gmail.com>
2024-06-28 08:26:30 +0400

    add routes admin constraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
+require 'sidekiq/web'
+
+class AdminConstraint
+  def matches?(request)
+    user_id = request.session[:user_id]
+
+    User.find_by(id: user_id)&.admin_role?
+  end
+end
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => 'jobs', constraints: AdminConstraint.new
+
   resource :session, only: %i[new create destroy]
   resources :users, only: %i[new create edit update]
 
-  namespace :admin do
+  namespace :admin, constraints: AdminConstraint.new do
     resources :users
   end
 
