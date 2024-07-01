7f448f5cd5147de1bc28899a0d71ac7efeb4eccd
Ilmir <code.for.func@gmail.com>
2024-07-01 22:04:16 +0400

    remove extra

--- app/jobs/update_post_status_job.rb
+++ app/jobs/update_post_status_job.rb
@@ -2,7 +2,6 @@ class UpdatePostStatusJob < ApplicationJob
   queue_as :default
 
   def perform(post_id, event)
-    puts '*' * 1000
     post = Post.find(post_id)
     Rails.logger.info("Start updating status: #{post.status}, event: #{event}")
     UpdatePostStatusService.call(post, event)
